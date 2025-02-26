4d1ed957de344de8a725a38f6f214d32e2168565
anurag jaiswal <anuraggjaiswal@gmail.com>
2025-02-26 12:22:08 +0530

    apiOfInterview

---
+++ actions/interview.js
@@ -0,0 +1,146 @@
+"use server"
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server"
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash"
+
+})
+
+//apis for mock intereview
+export async function generateQuiz() {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized")
+
+    const user = await db.user.create({
+        where: {
+            clerkUserId: userId
+        }
+    })
+    if (!user) throw new Error("User Not Found");
+
+
+
+
+    try {
+        //prompt for generating our interview Quiz
+        const prompt = `
+        Generate 10 technical interview questions for a ${user.industry
+            } professional${user.skills?.length ? ` with expertise in ${user.skills.join(", ")}` : ""
+            }.
+        
+        Each question should be multiple choice with 4 options.
+        
+        Return the response in this JSON format only, no additional text:
+        {
+          "questions": [
+            {
+              "question": "string",
+              "options": ["string", "string", "string", "string"],
+              "correctAnswer": "string",
+              "explanation": "string"
+            }
+          ]
+        }
+      `;
+
+        //now to structure or reponse
+        const result = await model.generateContent(prompt)
+        const response = result.response;
+        const text = response.text();
+
+        //clean the response
+        const cleanedText = text.replace(/```(?:json)?\n?/g, "").trim();
+        const quiz = JSON.parse(cleanedText)
+
+        return quiz.questions;
+    } catch (error) {
+        console.error("Error Generating Quiz", error)
+        throw new Error("failed to generate Quiz questions")
+    }
+
+}
+
+
+// to save the result of the quiz
+export async function saveQuizResult(questions, answers, score) {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized")
+
+    const user = await db.user.create({
+        where: {
+            clerkUserId: userId
+        }
+    })
+    if (!user) throw new Error("User Not Found");
+
+
+    const questionResults = questions.map((q, index) => ({
+        question: q.question,
+        answer: q.correctAnswer,
+        userAnswer: answers[index],
+        isCorrect: q.correctAnswer === answers[index],
+        explanation: q.explanation,
+    }))
+
+
+    const wrongAnswer = questionResults.filter((q) => !q.isCorrect);
+    let improvementTip=null;
+
+    //now feed it to gemini
+
+    if (wrongAnswer.length > 0) {
+        const wrongQuestionsText = wrongAnswer.map((q) =>
+            `Question: "${q.question}"\nCorrect Answer: "${q.answer}"\nUser Answer: "${q.userAnswer}"`
+        )
+            .join("\n\n");
+
+
+
+        const improvementPrompt = `
+      The user got the following ${user.industry} technical interview questions wrong:
+
+      ${wrongQuestionsText}
+
+      Based on these mistakes, provide a concise, specific improvement tip.
+      Focus on the knowledge gaps revealed by these wrong answers.
+      Keep the response under 2 sentences and make it encouraging.
+      Don't explicitly mention the mistakes, instead focus on what to learn/practice.
+    `;
+
+
+    //now lets call the gemini api
+
+        try {
+             //now to structure or reponse
+        const result = await model.generateContent(improvementPrompt)
+        const response = result.response;
+         improvementTip = response.text().trim();
+
+
+        } catch (error) {
+            console.error("Error generating tip:",error)
+        }
+    }
+
+    //now lets store it in our database feed it to our asseesment table
+    try {
+        const asseesment= await db.create({
+            data:{
+                userId:user.id,
+                quizScore:score,
+                questions:questionResults,
+                category:"Technical",
+                improvementTip,
+            }
+        });
+        return asseesment;
+    } catch (error) {
+        console.error("Error saving Quiz result",error)
+        throw new Error("failed to save result")
+    }
+}
