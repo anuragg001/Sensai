e14201cb7f0defef24e58bb7b2e8bdbdecdd5cbb
anurag jaiswal <anuraggjaiswal@gmail.com>
2025-02-27 17:50:40 +0530

    Quizlist

--- app/(main)/interview/_components/quiz-list.jsx
+++ app/(main)/interview/_components/quiz-list.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+"use client"
+import { useRouter } from 'next/navigation'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import React, { useState } from 'react'
+import { Button } from '@/components/ui/button';
+import { format } from 'date-fns';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { AlertDialogHeader } from '@/components/ui/alert-dialog';
+import QuizResult from './quiz-result';
 
-const QuizList = () => {
+const QuizList = ({ assessments }) => {
+  const router = useRouter();
+  const [selectedQuiz, setSelectedquiz] = useState(null)
+
   return (
-    <div>QuizList</div>
+    <>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between">
+          <div>
+            <CardTitle className="gradient-title text-3xl md:text-4xl">Recent Quiz</CardTitle>
+            <CardDescription>Review your past quiz performance</CardDescription>
+          </div>
+          <Button
+            onClick={() => router.push("/interview/mock")}
+          >Start new Quiz</Button>
+        </CardHeader>
+        <CardContent>
+          <div className='space-y-4'>
+            {assessments.map((assessment, index) => {
+              return (
+                <Card className="cursor-pointer hover:bg-muted/50 transition-colors"
+                  key={assessment.id}
+                  onClick={() => setSelectedquiz(assessment)}
+                >
+                  <CardHeader>
+                    <CardTitle>Quiz {index + 1}</CardTitle>
+                    <CardDescription className="flex justify-between w-full">
+                      <div>
+                        Score: {assessment.quizScore.toFixed(1)}%
+                      </div>
+                      <div>
+                        {format(new Date(assessment.createdAt), "MMMM dd,yyyy HH:mm")}
+                      </div>
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <p className='text-sm text-muted-foreground'> {assessment.improvementTip}</p>
+                  </CardContent>
+
+                </Card>
+
+              )
+            })}
+          </div>
+        </CardContent>
+
+      </Card>
+
+      {/*Dialog*/}
+
+      <Dialog open={!!selectedQuiz} onOpenChange={() => setSelectedquiz(null)}>
+        <DialogContent className="max-w-3xl max-h-[90vh] overflow-y-auto ">
+          <DialogHeader>
+            <DialogTitle></DialogTitle>
+          </DialogHeader>
+          <QuizResult
+          result={selectedQuiz}
+          onStartNew={()=>router.push("/interview/mock")}
+          hideStartNew
+          />
+        </DialogContent>
+      </Dialog>
+
+    </>
   )
 }
 
