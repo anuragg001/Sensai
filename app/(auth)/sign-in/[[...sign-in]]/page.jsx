7b33d3e7e01d85c94cafe1112b9541068d1128d4
anurag jaiswal <anuraggjaiswal@gmail.com>
2025-03-06 21:54:45 +0530

    -

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -10,3 +10,32 @@ const Page = () => {
 }
 
 export default Page
+
+
+// //"use client"
+// import { SignIn, useAuth } from '@clerk/nextjs'
+// import { useRouter } from 'next/navigation'
+// import { useEffect } from 'react'
+
+// const Page = () => {
+//   const { isSignedIn } = useAuth()
+//   const router = useRouter()
+
+//   useEffect(() => {
+//     if (isSignedIn) {
+//       router.push('/dashboard') // Redirect to dashboard or any other page
+//     }
+//   }, [isSignedIn, router])
+
+//   if (isSignedIn) {
+//     return null // Avoid rendering <SignIn /> if already signed in
+//   }
+
+//   return (
+//     <div>
+//       <SignIn />
+//     </div>
+//   )
+// }
+
+// export default Page
